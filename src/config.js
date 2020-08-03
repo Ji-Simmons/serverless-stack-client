f274bf06c1f50d135a74aa35f7090d488f3c3248
Ji Simmons <ji.simmons@yahoo.com>
2020-08-02 21:28:02 -0600

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://lfaxcjefu1.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_yyXIFPeiW",
+      APP_CLIENT_ID: "2o9aiosntrvbd7krj95pma3rse",
+      IDENTITY_POOL_ID: "us-east-2:19f8dbc8-53fb-4b3c-bed3-d859da557586"
+    }
+  };
